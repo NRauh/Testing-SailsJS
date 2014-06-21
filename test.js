9f969fc3a26f28802e131d68dd46fd6360b51dfc
Nate Rauh <NRauh@users.noreply.github.com>
2014-06-20 23:23:26 -0500

    Forgot stuff

--- test.js
+++ test.js
@@ -16,14 +16,15 @@ before(function(done) {
 
 describe("Foo", function() {
 	describe("bar", function() {
-		it("returns some JSON", function() {
+		it("returns some JSON", function(done) {
 			request(app).get("/foo/bar").end(function(err, res) {
 				expect(res.body.hello).to.equal("World");
+				done();
 			});
 		});
 	});
 });
 
-after(function() {
+after(function(done) {
 	// stuff to be done
 });
